ef10844cd0c1fe93ef77a59c6f039e111c4e3c5a
Kristian Windsor <kristianwindso@gmail.com>
2019-07-06 21:05:34 -0700

    simulation shows real prices

--- backend/src/simulation/runSimulation.py
+++ backend/src/simulation/runSimulation.py
@@ -2,6 +2,7 @@
 import os
 import pymysql
 from datetime import date, timedelta
+import indicators
 
 
 db = pymysql.connect(
@@ -12,6 +13,18 @@ db = pymysql.connect(
 	autocommit=True
 )
 cursor = db.cursor(pymysql.cursors.DictCursor)
+
+stockPrices = {}
+
+
+def portfolioNetWorth(portfolio):
+	netWorth = portfolio['cash']
+	for ticker in portfolio:
+		if ticker != 'cash':
+			quantity = float(portfolio[ticker])
+			price = float(stockPrices[ticker][max(stockPrices['AAPL'])])
+			netWorth += quantity * price
+	return netWorth
 
 
 def main(stock, indicators, startDate, endDate, cash):
@@ -28,17 +41,23 @@ def main(stock, indicators, startDate, endDate, cash):
 
 	delta = endDate - startDate
 
+	# get stock prices
+	cursor.execute("SELECT close_price, date FROM stock_data WHERE ticker = '" + stock + "' AND date >= '" + startDate.strftime('%Y-%m-%d') + "' AND date <= '" + endDate.strftime('%Y-%m-%d') + "'")
+	stockPrices[stock] = {}
+	for row in cursor.fetchall():
+		stockPrices[stock][row['date'].strftime('%Y-%m-%d')] = row['close_price']
+	print(stockPrices)
+
 	# run simulation
 	portfolio = { 'cash': cash }
-	portfolioNetWorth = cash
 	for i in range(delta.days + 1):
 		date = (startDate + timedelta(days=i))
-		if date.weekday() < 5:
+		if date.weekday() < 5 and date.strftime('%Y-%m-%d') in stockPrices[stock]:
 			print(date.strftime('%Y-%m-%d'))
-			portfolioNetWorth += 5
+			portfolio['cash'] += 5
 			for indicator in indicators:
 				print('indicator:' + indicator)
-				stockPrice = 204.23
+				stockPrice = float(stockPrices[stock][date.strftime('%Y-%m-%d')])
 				indicatorValue = 7
 				if stockPrice < (portfolio['cash'] * indicatorValue * 0.01):
 					if stock not in portfolio:
@@ -55,11 +74,7 @@ def main(stock, indicators, startDate, endDate, cash):
 					portfolio['cash'] -= stockPrice * quantity
 					results['transactions'].append(transaction)
 			print(portfolio)
-			portfolioNetWorth = portfolio['cash']
-			for asset in portfolio:
-				if asset != 'cash':
-					portfolioNetWorth += int(portfolioNetWorth) + (int(portfolio[asset]) * int(stockPrice))
-			results['chartData'][date.strftime('%Y-%m-%d')] = portfolioNetWorth
+			results['chartData'][date.strftime('%Y-%m-%d')] = portfolioNetWorth(portfolio)
 
 	return results
 
