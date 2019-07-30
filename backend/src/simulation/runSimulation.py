fd19e333f84e1008d2976501f2e56e1344e01330
Kristian Windsor <kristianwindso@gmail.com>
2019-07-30 00:10:07 -0700

    implemented celery for parallel tasks

--- backend/src/simulation/runSimulation.py
+++ backend/src/simulation/runSimulation.py
@@ -3,6 +3,9 @@ import os
 import pymysql
 from datetime import datetime, date, timedelta
 import indicators
+from celery import Celery
+
+cel = Celery('celery_blog', broker='redis://localhost:6379/0')
 
 
 stockPrices = {}
@@ -22,21 +25,24 @@ def portfolioNetWorth(portfolio, date):
 
 
 def percentDifference(no1, no2):
-	return ((no1 - no2) / ((no1 + no2) / 2 )) * 100 * (-1)
+	diff = ((no1 - no2) / ((no1 + no2) / 2 )) * 100 * (-1)
+	return diff
 
 
 def closestDate(givenDate, dateList):
 	returnDate = None
 	for d in dateList:
 		d = datetime.strptime(d, '%Y-%m-%d')
-		if d < givenDate:
+		if d <= givenDate:
 			if returnDate == None or d > returnDate:
 				returnDate = d
 	return returnDate
 
 
 def getStockPrice(ticker, date):
-	return float(stockPrices[ticker][closestDate(date, stockPrices[ticker]).strftime('%Y-%m-%d')])
+	dateString = closestDate(date, stockPrices[ticker]).strftime('%Y-%m-%d')
+	stockPrice = stockPrices[ticker][dateString]
+	return float(stockPrice)
 
 
 def main(stock, indicatorSettings, startDate, endDate, cash):
@@ -69,78 +75,105 @@ def main(stock, indicatorSettings, startDate, endDate, cash):
 	for row in cursor.fetchall():
 		stockPrices[stock][row['date'].strftime('%Y-%m-%d')] = row['close_price']
 
-	# run simulation
-	portfolio = { 'cash': cash }
-	startStockPrice = None
+	# get average indicator value
+	indicatorChartData = {}
+	averageIndicatorValues = {}
 	for i in range(delta.days + 1):
 		date = (startDate + timedelta(days=i))
-		if date.weekday() < 5 and date.strftime('%Y-%m-%d') in stockPrices[stock]:
-			# get indicator values
-			averageIndicatorValue = 0
-			numerator = 0
-			denominator = 0
-			indicatorChartData = {}
-			if stock not in portfolio:
-				portfolio[stock] = {
-					'shares': 0,
-					'shorting': []
-				}
-			#
-			stockPrice = float(getStockPrice(stock, date))
-			if not startStockPrice:
-				startStockPrice = stockPrice
-			for indicatorName in indicatorSettings:
-				indicatorValue = float(getattr(indicators, indicatorName).main(stock, date.strftime('%Y-%m-%d')))
-				weight = indicatorSettings[indicatorName]
-				numerator += indicatorValue * weight
-				denominator += abs(weight)
-				indicatorChartData[indicatorName] = indicatorValue * weight
-			if denominator != 0:
-				averageIndicatorValue = (numerator / denominator) * 0.01 
-				# buy / sell
-			fundsAllocated = portfolio['cash'] + (stockPrice * portfolio[stock]['shares'])
-			for shortedPrice in portfolio[stock]['shorting']:
-				fundsAllocated -= stockPrice
-				fundsAllocated += shortedPrice
-			quantityPossible = int(fundsAllocated / stockPrice)
-			quantityShouldHave = int(quantityPossible * averageIndicatorValue)
-			currentQuantity = portfolio[stock]['shares'] - len(portfolio[stock]['shorting'])
-			quantityToMove = quantityShouldHave - currentQuantity
-			if quantityToMove > 0:
-				# bull
-				while quantityToMove != 0:
-					if len(portfolio[stock]['shorting']) > 0:
-						# buy back short
-						portfolio['cash'] -= stockPrice
-						portfolio['cash'] += portfolio[stock]['shorting'][0]
-						portfolio[stock]['shorting'].pop(0)
-					else:
-						# buy shares
-						portfolio[stock]['shares'] += 1
-						portfolio['cash'] -= stockPrice
-					quantityToMove -= 1
-			elif quantityToMove < 0:
-				# bear
-				while quantityToMove != 0:
-					if portfolio[stock]['shares'] > 0:
-						# sell shares
-						portfolio[stock]['shares'] -= 1
-						portfolio['cash'] += stockPrice
-					else:
-						# sell short
-						portfolio[stock]['shorting'].append(stockPrice)
-					quantityToMove += 1
-			#
-			results['chartData'][date.strftime('%Y-%m-%d')] = {
-				'portfolioNetWorth': portfolioNetWorth(portfolio, date),
-				# 'portfolioNetWorthPercent': portfolioNetWorth(portfolio, date) / cash * 100 - 100,
-				'portfolioNetWorthPercent': percentDifference(cash, portfolioNetWorth(portfolio, date)),
-				'stockPrice': stockPrice,
-				'stockPricePercent': stockPrice / startStockPrice * 100 - 100,
-				'stockQuantity': portfolio[stock]['shares'] - len(portfolio[stock]['shorting']),
-				'indicators': indicatorChartData
-			}
-	results['gain'] = percentDifference(cash, portfolioNetWorth(portfolio, date))
-
+		dateString = date.strftime('%Y-%m-%d')
+		if date.weekday() < 5 and date.strftime(dateString) in stockPrices[stock]:
+			indicatorData = getIndicatorValues(stock, indicatorSettings, date, cash)
+			indicatorChartData[dateString] = indicatorData['indicatorChartData']
+			averageIndicatorValues[dateString] = indicatorData['averageIndicatorValue']
+	
+	# run simulation
+	print(averageIndicatorValues)
+	print(indicatorChartData)
+	startStockPrice = getStockPrice(stock, startDate)
+	portfolio = { 'cash': cash }
+	if stock not in portfolio:
+		portfolio[stock] = {
+			'shares': float(0)	,
+			'shorting': []
+		}
+	
+	for dateString in averageIndicatorValues:
+		date = datetime.strptime(dateString, '%Y-%m-%d')
+		# print(stockPrices)
+		stockPrice = float(stockPrices[stock][dateString])
+		fundsAllocated = portfolio['cash'] + (stockPrice * portfolio[stock]['shares'])
+		for shortedPrice in portfolio[stock]['shorting']:
+			fundsAllocated -= stockPrice
+			fundsAllocated += shortedPrice
+		quantityPossible = int(fundsAllocated / stockPrice)
+		quantityShouldHave = int(quantityPossible * averageIndicatorValues[dateString])
+		currentQuantity = portfolio[stock]['shares'] - len(portfolio[stock]['shorting'])
+		quantityToMove = quantityShouldHave - currentQuantity
+		if quantityToMove > 0:
+			# bull
+			while quantityToMove != 0:
+				if len(portfolio[stock]['shorting']) > 0:
+					# buy back short
+					portfolio['cash'] -= stockPrice
+					portfolio['cash'] += portfolio[stock]['shorting'][0]
+					portfolio[stock]['shorting'].pop(0)
+				else:
+					# buy shares
+					portfolio[stock]['shares'] += 1
+					portfolio['cash'] -= stockPrice
+				quantityToMove -= 1
+		elif quantityToMove < 0:
+			# bear
+			while quantityToMove != 0:
+				if portfolio[stock]['shares'] > 0:
+					# sell shares
+					portfolio[stock]['shares'] -= 1
+					portfolio['cash'] += stockPrice
+				else:
+					# sell short
+					portfolio[stock]['shorting'].append(stockPrice)
+				quantityToMove += 1
+		print(portfolio)
+		#
+		results['chartData'][date.strftime('%Y-%m-%d')] =  {
+			'portfolioNetWorth': portfolioNetWorth(portfolio, date),
+			'portfolioNetWorthPercent': percentDifference(cash, portfolioNetWorth(portfolio, date)),
+			'stockPrice': stockPrice,
+			'stockPricePercent': stockPrice / startStockPrice * 100 - 100,
+			'stockQuantity': portfolio[stock]['shares'] - len(portfolio[stock]['shorting']),
+			'indicators': indicatorChartData[date.strftime('%Y-%m-%d')]
+		}
+		
+	#
+	firstDate = None
+	lastDate = None
+	for dateString in results['chartData']:
+		date = datetime.strptime(dateString, '%Y-%m-%d')
+		if firstDate == None or date < firstDate:
+			firstDate = date
+		if lastDate == None or date > lastDate:
+			lastDate = date
+	results['gain'] = results['chartData'][lastDate.strftime('%Y-%m-%d')]['portfolioNetWorthPercent']
+	
+	print(stockPrices)
 	return results
 
+
+@cel.task
+def getIndicatorValues(stock, indicatorSettings, date, cash):
+	averageIndicatorValue = 0
+	numerator = 0
+	denominator = 0
+	indicatorChartData = {}
+	for indicatorName in indicatorSettings:
+		indicatorValue = float(getattr(indicators, indicatorName).main(stock, date.strftime('%Y-%m-%d')))
+		weight = indicatorSettings[indicatorName]
+		numerator += indicatorValue * weight
+		denominator += abs(weight)
+		indicatorChartData[indicatorName] = indicatorValue * weight
+	if denominator != 0:
+		averageIndicatorValue = (numerator / denominator) * 0.01 
+	return {
+		'averageIndicatorValue': averageIndicatorValue,
+		'indicatorChartData': indicatorChartData
+	}
