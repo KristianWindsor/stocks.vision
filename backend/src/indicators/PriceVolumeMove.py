d0c8f31b74c429aee4dc7473a873887d0ae957c0
KristianWindsor <kristianwindso@gmail.com>
2021-03-19 05:28:45 -0700

    improved PriceVolumeMove indicator

--- backend/src/indicators/PriceVolumeMove.py
+++ backend/src/indicators/PriceVolumeMove.py
@@ -5,9 +5,44 @@ from datetime import datetime, timedelta
 import dateutil.relativedelta
 import requests
 import json
+from decimal import Decimal
+from indicators import VolumeUp
 
 
-def main(stock, date):
+def max(numbers):
+	highest = None
+	for n in numbers:
+		if highest == None:
+			highest = n
+		elif n > highest:
+			highest = n
+	return highest
+
+
+def min(numbers):
+	lowest = None
+	for n in numbers:
+		if lowest == None:
+			lowest = n
+		elif n < lowest:
+			lowest = n
+	return lowest
+
+
+def normalize(n, numberList):
+	return Decimal((n - min(numberList)) / (max(numberList) - min(numberList)) * (100 - -100) + -100)
+
+
+def closestDate(givenDate, dateList):
+	returnDate = None
+	for d in dateList:
+		if d <= givenDate.date():
+			if returnDate == None or d > returnDate:
+				returnDate = d
+	return returnDate
+
+
+def main(stock, givenDate):
 	db = pymysql.connect(
 		host=os.environ['MYSQL_HOSTNAME'],
 		user='backend',
@@ -16,23 +51,43 @@ def main(stock, date):
 		autocommit=True
 	)
 	cursor = db.cursor(pymysql.cursors.DictCursor)
-	startDate = datetime.strptime(date, "%Y-%m-%d")
-	startDate = startDate - dateutil.relativedelta.relativedelta(days=1)
-	startDate = startDate.strftime('%Y-%m-%d')
+	givenDate = datetime.strptime(givenDate, "%Y-%m-%d")
+	startDate = givenDate - dateutil.relativedelta.relativedelta(weeks=2)
 	# get data
-	cursor.execute("SELECT * FROM stock_data WHERE ticker = '" + stock + "' AND date >= '" + startDate + "' AND date <= '" + date + "'")
+	sql = "SELECT * FROM stock_data WHERE ticker = '" + stock + "' AND date >= '" + startDate.strftime('%Y-%m-%d') + "' AND date <= '" + givenDate.strftime('%Y-%m-%d') + "'"
+	print(sql)
+	cursor.execute(sql)
 	rows = cursor.fetchall()
 	lastPriceDifference = None
-	lastVolume = None
+	priceJumpList = []
+	dateList = []
+	priceVolumeMap = {}
 	result = 0
 	for row in rows:
 		priceDifference = row['close_price'] - row['open_price']
-		volume = row['volume']
-		if lastPriceDifference and lastVolume:
-			if volume > lastVolume and priceDifference > 0:
-				result = 100
-			elif volume > lastVolume and priceDifference < 0:
-				result = -100
+		if lastPriceDifference:
+			priceJump = abs(priceDifference)
+			positiveDirection = priceDifference >= 0
+			priceVolumeMap[row['date'].strftime('%Y-%m-%d')] = {
+				'priceJump': priceJump,
+				'positiveDirection': positiveDirection
+			}
+			priceJumpList.append(priceJump)
+			dateList.append(row['date'])
 		lastPriceDifference = priceDifference
-		lastVolume = volume
-	return str(result)
+
+	date = closestDate(givenDate, dateList)
+	priceJump = priceVolumeMap[date.strftime('%Y-%m-%d')]['priceJump']
+	positiveDirection = priceVolumeMap[date.strftime('%Y-%m-%d')]['positiveDirection']
+	volumeResult = Decimal(VolumeUp.main(stock, date.strftime('%Y-%m-%d')))
+	priceResult = normalize(priceJump, priceJumpList)
+	try:
+		result = volumeResult * priceResult / 100
+		print(result)
+		if not positiveDirection:
+			result = result * -1
+		# print('\nshares ('+str(int(volumeResult))+'%) and price is up ' + str(priceJump) + ' ('+str(int(priceResult))+'%) (positive: '+str(positiveDirection)+'), so the move is ' + str(result) + '\n')
+		return str(result)
+	except:
+		print('[WARNING][Volume] couldn\'t find matching volume for ' + givenDate.strftime('%Y-%m-%d'))
+		return '0'
